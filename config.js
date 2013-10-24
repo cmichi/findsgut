2e87325d3c8deb5af354c620d30e7370e7534470
cmichi <mich@elmueller.net>
2013-10-24 16:40:16 +0200

    adding config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports.mail = {
+	host: 'smtp.foo.bar',
+	user: 'foo',
+	password: "bar", 
+	tls: true,
+
+	sender: "info@findsgut.de"
+};
+
+//module.exports.session_secret = '5acfbfe3d02942ecbef51a5d26e08a325c6f0930';
