9847064ac2c6d8979f75c2c2bca184b832eea89e
cmichi <mich@elmueller.net>
2013-10-24 20:37:28 +0200

    massively working on layout and error reporting

--- findsgut.js
+++ findsgut.js
@@ -33,7 +33,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/entries/all', function(req, res) {
-	res.render('entries/all', layout.get_vars('entries'));
+	res.render('entries/all', layout.get_vars('entries_all'));
 });
 
 app.get('/impressum', function(req, res) {
@@ -44,9 +44,125 @@ app.get('/kontakt', function(req, res) {
 	res.render('kontakt', layout.get_vars('feedback'));
 });
 
-app.get('/neu', function(req, res) {
-	res.render('entries/new', layout.get_vars('entries'));
+var error_fields = {
+	  name: ""
+	, uri: ""
+	, categories: ""
+	, classification: ""
+	, online_local: ""
+	, address: ""
+	, description: ""
+	, agb: ""
+}
+app.get('/entries/new', function(req, res) {
+	res.render('entries/new', layout.get_vars('entries_new',
+	{"error_fields": error_fields}));
 });
+
+app.post('/entries/new', function(req, res) {
+	console.log(JSON.stringify(req.body));
+	console.log("\n");
+
+	var validation_results = validate(req.body, error_fields);
+	var validator = validation_results.validator;
+	var errors = validator.getErrors();
+
+	if (errors != undefined && errors.length > 0) {
+		//console.log(errors);
+		// render site again, show errors, show previously entered input
+		var additional_params = {
+			  "errors": errors
+			, previous_input: req.body
+			, error_fields: validation_results.error_fields
+		};
+
+		res.render('entries/new', layout.get_vars('entries_new', additional_params));
+		return;
+	} else {
+		newEntry(res, req.body)
+	}
+});
+
+function newEntry(res, body) {
+	var new_obj = {
+		  type: "entry"
+		, beschreibung: body.inputDescription + "bar"
+		, created_at: (new Date().getTime())
+	};
+
+	db.save(new_obj, function(err, res_created) {
+		if (err) {
+			// show error note 
+			// render site with previous input
+			console.log(JSON.stringify(err));
+		}
+
+		//console.log(JSON.stringify(res_created));
+		
+		//getSlug(obj.topic, function(slug) {
+			//res.redirect('/de/entry/' + slug + "?opinion_added");
+		//});
+	});
+}
+
+function prepare(foo) {
+	var bar = sanitize(foo).xss();
+	bar = sanitize(bar).escape();
+	bar = sanitize(bar).trim();
+
+	return bar;
+}
+
+function validate(body, error_fields) {
+	var Validator = require('validator').Validator;
+
+	Validator.prototype.error = function (msg) {
+		this._errors.push(msg);
+		return this;
+	}
+
+	Validator.prototype.getErrors = function () {
+		return this._errors;
+	}
+
+	var validator = new Validator();
+	var check = validator.check;
+	
+	var name = prepare(body.name)
+	var chk = validator.check(name, "Bitte geben Sie einen Namen an.").notEmpty();
+	if (chk.msg != "") 
+		error_fields.name = "has-error";
+		
+	var description = prepare(body.description)
+	var chk = validator.check(description, "Bitte geben Sie eine Beschreibung an.").notEmpty();
+	if (chk.msg != "") 
+		error_fields.description = "has-error";
+
+	var uri = prepare(body.uri);
+	if (body.local_online && uri === "") {
+		validator.error("Bei Online-Angeboten ist die Angabe einer Internet-Adresse verpflichtend.");
+	}
+	if (uri != "") {
+		var chk = validator.check(body.uri, "Bitte überprüfen Sie die Internetadresse").isUrl();
+		if (chk.msg != "") 
+			error_fields.uri = "has-error";
+	}
+
+	var chk = validator.check(body.online_local, "Bitte geben Sie an, ob das Angebot online oder lokal ist.").notEmpty();
+	if (chk.msg != "") 
+		error_fields.online_local = "has-error";
+
+
+
+	var chk = validator.check(body.agb, "Bitte akzeptieren Sie die AGB.").equals("on");
+	if (chk.msg != "") 
+		error_fields.agb = "has-error";
+
+	return {
+		"validator": validator
+		, "error_fields": error_fields
+	};
+} 
 
 app.post('/kontakt', function(req, res) {
 	var content = "Name:\t" + req.param('inputName') + "\n";
--- layout.js
+++ layout.js
@@ -1,6 +1,7 @@
 var layout_vars = {
 	navi: {}
 	, count_entries: 0
+	, categories: []
 };
 
 var db;
@@ -13,31 +14,50 @@ exports.get_vars = function(navi_key, additional_vars) {
 		vars[i] = additional_vars[i];
 	}
 
+	console.log(JSON.stringify(vars));
+
 	return vars;
 }
 
 exports.init = function(d) {
 	db = d;
 
-	db.view('couchapp/entries', {reduce: false}, function (err, res) {
+	db.view('db/entries', {reduce: false}, function (err, res) {
 		if (err) {
 			console.dir(err);
 			return;
 		}
 
 		layout_vars.count_entries = res.length;
+		layout_vars.entries = res;
+	});
+
+	db.view('db/categories', {reduce: false}, function (err, res) {
+		if (err) {
+			console.dir(err);
+			return;
+		}
+
+		if (res.length > 0)
+			layout_vars.categories = res
+			//layout_vars.categories = res[0].value;
+
+		console.log(JSON.stringify(layout_vars.categories))
 	});
 }
 
 function get_navi(k, req) {
 	var navi = {
 		  home: ""
-		, entries: ""
+		, entries_new: ""
+		, entries_all: ""
 		, feedback: ""
 	};
 
-	if (k === "entries")
-		navi.entries = "active";
+	if (k === "entries_new")
+		navi.entries_new = "active";
+	else if (k === "entries_all")
+		navi.entries_all = "active";
 	else if (k === "feedback")
 		navi.feedback = "active";
 	else
