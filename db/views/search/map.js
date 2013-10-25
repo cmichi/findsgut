ccacd95122581e733b48b8d9b400e273f1d36c2f
cmichi <mich@elmueller.net>
2013-10-25 12:11:02 +0200

    fixed error in search view

--- db/views/search/map.js
+++ db/views/search/map.js
@@ -1,8 +1,9 @@
 function(doc) {
 	if (doc.type != "entry") return;
 
-	var txt = doc.name + doc.description + doc.address.join(" ");
+	var txt = doc.name + " " + doc.description + " " + doc.address;
 	var words = txt.replace(/[!.,;?]+/g," ").toLowerCase().split(" ");
+
 	for (var word in words) {
 		if (words[word].replace(/\s/g, "").length > 0) 
 		emit(words[word], doc);
