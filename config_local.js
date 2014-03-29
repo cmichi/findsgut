6e82fba935a3bc1e4595417465d8102d95f4a254
findsgut <findsgut@mail.serenity.browncoats.eu>
2014-03-29 14:33:24 +0100

    Adjusted the mail sending configuration (localhost + to mailinglist)

---
+++ config_local.js
@@ -0,0 +1,12 @@
+module.exports.mail = {
+	host: 'localhost',
+	user: 'findsgut',
+	password: "", 
+	tls: false,
+
+	sender: "Website Feedback <findsgut@marmaro.de>",
+	feedback_to: "findsgut@marmaro.de",
+	admin_mail: "mich@elmueller.net"
+};
+
+module.exports.session_secret = '5acfbfe3d02942ecbef51a5d26e08a325c6f0930';
