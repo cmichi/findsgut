2da850268ea7a95a9fc2209752c3c56b5d9548e5
findsgut <findsgut@mail.serenity.browncoats.eu>
2014-03-30 15:40:09 +0200

    dont log updates

--- static/js/entries/new.js
+++ static/js/entries/new.js
@@ -1,5 +1,4 @@
 var updateForm = function() { 
-	console.log("update");
 	/* possible bug here: when form is saved with local=false, but 
 	returns with an error: is the address field still hidden? */
 	var local = $("form[id=new] input[name=local]").prop("checked");
