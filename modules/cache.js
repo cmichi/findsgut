d87bec0af0f233f90f858bef5fcb6ae3e95b477f
cmichi <mich@elmueller.net>
2014-04-07 00:41:03 +0200

    a get request is executed for various uris once the app starts up, in order to make sure the cache gets filled

--- modules/cache.js
+++ modules/cache.js
@@ -1,7 +1,8 @@
+var https = require('https');
 var db;
 var entries = 0;
 
-refreshEntries = function(cb) {
+var refreshEntries = function(cb) {
 	db.view('db/entries', {reduce: false}, function (err, res_entries) {
 		if (err) {
 			layout.error(500, err, null, null, layout.get_vars('entries_all'));
@@ -11,6 +12,35 @@ refreshEntries = function(cb) {
 		entries = res_entries;
 		console.log("loaded " + res_entries.length + " entries into cache");
 	});
+}
+
+/* execute get request for those uris in order to make sure
+the cache gets initialized */
+exports.pingCache = function() {
+	var prefix = "https://findsgut.de";
+	var uris = [
+		'/', 
+		'/eintraege/neu', 
+		'/eintraege/alle', 
+		'/feedback', 
+		'/idee',
+		'/impressum'
+	];
+
+	for (var u in uris) {
+		var uri =  prefix + uris[u];
+		(function(uri) {
+			console.log("https.get for " + uri);
+
+			https.get(uri, function(res) {
+				console.log("Got response: " + res.statusCode 
+					+ " for " + uri);
+			}).on('error', function(e) {
+				console.log("Got error: " + e.message 
+					+ " for " + uri);
+			});
+		})(uri);
+	}
 }
 
 exports.refresh = function() {
@@ -27,4 +57,9 @@ exports.getEntries = function(cb) {
 exports.init = function(_db) {
 	db = _db;
 	this.refresh();
+
+	if (process.env.NODE_ENV == 'production') {
+		this.pingCache();
+		this.pingCache();
+	}
 }
