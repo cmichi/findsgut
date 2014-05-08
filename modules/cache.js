ad8c1d6db8abb0bc2381da706b78a705026b9a4f
cmichi <mich@elmueller.net>
2014-05-08 15:57:36 +0200

    improved search. a lot.

--- modules/cache.js
+++ modules/cache.js
@@ -13,24 +13,74 @@ exports.search = function(foo, opts, cb) {
 
 	for (var e in entries) {
 		var entry = entries[e].value;
-		//console.log(JSON.stringify(entry, null, "\t"));
 		var searchTxt = assembleSearchTxt(entry);
-		//console.log(searchTxt);
-		//console.log("!" + term + "!");
 
 		var re = new RegExp(term, "gi");
-		console.log(re);
 		if (searchTxt.match(re)) {
 			results.push(entries[e]);
 		}
 	}
 
-	console.log(results.length);
 	cb(null, results);
 }
 
-var assembleSearchTxt = function(entry) {
-	return entry.name + entry.description;
+var assembleSearchTxt = function(doc) {
+	var address = "";
+	if (doc.street) address += doc.street + " ";
+	if (doc.city) address += doc.city + " ";
+	if (doc.zipcode) address += doc.zipcode + " ";
+
+	var txt = doc.name + " " + doc.description + " " + address;
+
+	if (doc.classifications) {
+		for (var c in model.classifications) {
+			for (var doc_c in doc.classifications) {
+				if (model.classifications[c].key == doc.classifications[doc_c])
+					txt += " " + model.classifications[c].value + " ";
+			}	
+		}
+	}
+
+	if (doc.categories) {
+		for (var c in model.categories) {
+			for (var doc_c in doc.categories) {
+				if (model.categories[c].key == doc.categories[doc_c])
+					txt += " " + model.categories[c].value + " ";
+			}	
+		}
+	}
+
+	txt += parseSub(doc.subcategories).join(" ");
+	txt = txt.replace(/\&[A-Za-z]+\;/gi, ' ');
+	txt = txt.replace(/\-/gi, '');
+	txt = txt.replace(/\(/gi, '');
+	txt = txt.replace(/\)/gi, '');
+	txt = txt.replace(/\+/gi, '');
+	txt = txt.replace(/\~/gi, '');
+	txt = txt.replace(/\-/gi, '');
+	txt = txt.replace(/\%/gi, '');
+
+	return txt;
+}
+
+/* return subcategories objects which are correct for subcategories_arr */
+function parseSub(subcategories_arr) {
+	var arr = [];
+
+	for (var c0 in subcategories_arr) {
+		for (var c1 in model.subcategories) {
+			for (var c2 in model.subcategories[c1]) {
+				var obj = model.subcategories[c1][c2];
+				for (var c3 in obj.list) {
+					var obj2 = obj.list[c3];
+					if (obj2.key === subcategories_arr[c0])
+						arr.push(obj2.value);
+				}
+			}
+		}
+	}
+
+	return arr;
 }
 
 var refreshAllEntriesCoords = function() {
